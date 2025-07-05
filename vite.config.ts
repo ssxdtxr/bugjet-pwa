639a1297882c950155f229c7f60f5605a7fd6922
ssxdtxr <tereskinegor68@gmail.com>
2025-07-05 17:43:52 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,74 @@
+import { ManifestOptions, VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const manifest: Partial<ManifestOptions> = {
+  theme_color: "#8936FF",
+  background_color: "#2EC6FE",
+  icons: [
+    {
+      purpose: "maskable",
+      sizes: "512x512",
+      src: "icon512_maskable.png",
+      type: "image/png",
+    },
+    {
+      purpose: "any",
+      sizes: "512x512",
+      src: "icon512_rounded.png",
+      type: "image/png",
+    },
+  ],
+  screenshots: [
+    {
+      src: "/screenshots/desktop.png",
+      type: "image.png",
+      sizes: "2746x1984",
+      form_factor: "wide",
+    },
+    {
+      src: "/screenshots/mobile.png",
+      type: "image.png",
+      sizes: "790x1688",
+      form_factor: "narrow",
+    },
+  ],
+  orientation: "any",
+  display: "standalone",
+  lang: "ru",
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.ts",
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{html, css, js, ico, png, svg}"],
+      },
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest,
+
+      injectManifest: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
